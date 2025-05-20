082b10ea102b031cc5a192ecb43a6bc05b6e2b77
Arcdarelle <darelle.ghomo@utrains.com>
2025-05-20 11:39:36 +0100

    Update the pipeline

--- ecr/network.tf
+++ ecr/network.tf
@@ -43,6 +43,14 @@ resource "aws_security_group" "frontend_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    description = "allows connection from the internet"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
